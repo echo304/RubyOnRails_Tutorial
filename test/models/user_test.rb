03069cde939fc6b7db2444235be2e5cfe8234704
S.B. Lee <echo3042@gmail.com>
2016-08-21 16:40:58 +0900

    (feat) make user model including secure password

---
+++ test/models/user_test.rb
@@ -0,0 +1,78 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = User.new(name: "Test Test", email: "test@test.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+    assert_equal @user.name, "Test Test"
+  end
+
+  test "name should not be blank" do
+    @user.name = "      "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be blank" do
+    @user.email = "      "
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com user@example..com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  test "email address should be uniq" do
+    duplicated_user = @user.dup
+    duplicated_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicated_user.valid?
+  end
+
+  test "email address should be saved as lower-case" do
+    uppercase = "UPPERCASE@TEST.COM"
+    @user.email = uppercase
+    @user.save
+    assert_equal uppercase.downcase, @user.reload.email
+  end
+
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = "        "
+    assert_not @user.valid?
+  end
+
+  test "password should have minimum length of 6" do
+    @user.password = @user.password_confirmation = "12345"
+    assert_not @user.valid?
+  end
+end
